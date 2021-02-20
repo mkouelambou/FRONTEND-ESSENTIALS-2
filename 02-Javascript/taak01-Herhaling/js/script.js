f73fa81d11b94fcd6fd371b9573c0c4c8a05586d
Mike <mike.kouelambou@student.rocva.nl>
2021-02-20 12:32:28 +0100

    save changes

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,18 @@
 let strNaam;
 let boolNaamKlopt;
+
+prompt("Hoe heet je?");
+alert("Je heet " + strNaam);
+boolNaamKlopt = confirm("Heet je echt " + strNaam +"?");
+prompt("Welkom Mike !")
+strNaam = prompt("Nog een keer: Hoe heet je?");
+boolNaamKlopt = confirm("Heet je echt " + strNaam +"?");
+
+if (test == "Hallo"){
+    //conditie tussen de () is TRUE, voer code hier uit
+}
+else{
+    //conditie tussen de () is FALSE, voer code hier uit
+    alert("Hallo, even goed welkom !");
+}
+
