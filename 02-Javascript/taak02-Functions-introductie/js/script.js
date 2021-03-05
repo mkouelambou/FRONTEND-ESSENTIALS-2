6f7a18eab65e700fb756d35f4e70c5e4c2bd4037
Mike <mike.kouelambou@student.rocva.nl>
2021-03-05 11:10:55 +0100

    save changes

--- 02-Javascript/taak02-Functions-introductie/js/script.js
+++ 02-Javascript/taak02-Functions-introductie/js/script.js
@@ -2,3 +2,17 @@ toonAlertBox();
 function toonAlertBox(){
     alert("This is my first message");
 }
+let Nickname;
+Nickname = prompt("What is your nickname?");
+if(Nickname == true){ 
+    alert("OK!");
+}
+else { 
+    Nickname = confirm("Is your name "+Nickname+"?");
+    if(Nickname == true){ 
+    alert("OK!");   
+    }
+    else{
+    alert("Dan niet!");   
+    }
+}
