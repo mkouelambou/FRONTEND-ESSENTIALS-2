7a49b27f2a7ed69284082c18b870c9e44be2fc07
Mike <mike.kouelambou@student.rocva.nl>
2021-03-09 17:46:09 +0100

    save changes

--- 02-Javascript/taak03-Functions-Return/js/script.js
+++ 02-Javascript/taak03-Functions-Return/js/script.js
@@ -1,5 +1,10 @@
-function geefRandomGetalTerug(){                
-    
+function geefRandomGetalTerug(){
+    let geefRandomGetalTerug = Math.random();
+    Math.random = randomGetal *100 + 1;
+    randomGetal = Math.floor(randomGetal); 
+    geefRandomGetalTerug = 10;
+    randomGetal = [10];
+    return 10;
 }
 
 console.log(geefRandomGetalTerug());    
