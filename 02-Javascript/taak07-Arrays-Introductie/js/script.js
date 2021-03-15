8d24ea25fa74e43386f6a163e42e50d7df212ec5
Mike <mike.kouelambou@student.rocva.nl>
2021-03-15 12:38:27 +0100

    commit

--- 02-Javascript/taak07-Arrays-Introductie/js/script.js
+++ 02-Javascript/taak07-Arrays-Introductie/js/script.js
@@ -1,1 +1,6 @@
-topWinkels = ["De Bijenkorf" ,"H&M" , "Zara"]
+topWinkels = ["De Bijenkorf" ,"H&M" , "Zara"];
+topGames = ["Fifa" , "Call of Duty" , "PUBG"];
+topFilms = ["The Shawshank Redemption" , "The Wizard of Oz" , "Olympus has fallen"];
+console.log(topWinkels[0]); console.log(topWinkels[1]); console.log(topWinkels[2]); 
+console.log(topFilms[0]); console.log(topFilms[1]); console.log(topFilms[2]); 
+console.log(topGames[0]); console.log(topGames[1]); console.log(topGames[2]); 
