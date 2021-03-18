61a617bd715a422018fa5e159249280be9793a3f
Mike <mike.kouelambou@student.rocva.nl>
2021-03-18 12:14:34 +0100

    save changes

--- 02-Javascript/taak09-Arrays-Toevoegen/js/script.js
+++ 02-Javascript/taak09-Arrays-Toevoegen/js/script.js
@@ -16,9 +16,24 @@ eigenTopFilms = [];
 //met deze for-loop word de ordered list gevuld met film namen
 for(i = 0; i < topFilms.length; i++){
     document.querySelector(".imdb-film" + (i+1)).innerHTML = topFilms[i];
-    kiesFilm = prompt("Kies film voor film ")
+    
 }
-
+var filmnummer=1; 
+ 
 function kiesFilm(){
-
-}
+    if(document.querySelector('.kiesfilm-button').innerText != 'KLAAR'){
+     nummer=prompt("Welke Film staat op plek " + filmnummer);
+     if(nummer>=0 && nummer<=10 ){
+       eigenTopFilms.push(topFilms[nummer]); 
+       naam=document.querySelector(".imdb-film"+nummer).innerHTML;
+       let toegevoegd1= document.querySelector(".eigen-film"+filmnummer);
+       toegevoegd1.innerHTML=naam;
+       console.log(naam);
+       document.querySelector(".imdb-film"+nummer).style.textDecoration = "line-through";
+       filmnummer++;
+       document.querySelector('.kiesfilm-button').innerText = 'Kies Film voor plek ' + filmnummer;
+       if (filmnummer>3){
+        document.querySelector('.kiesfilm-button').innerText = 'KLAAR';
+       }
+     }
+    }}
