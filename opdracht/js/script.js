e8ce2043d630b62126817001896e1ba15432e554
Mike <mike.kouelambou@student.rocva.nl>
2021-03-26 11:56:12 +0100

    save changes

--- opdracht/js/script.js
+++ opdracht/js/script.js
@@ -1,4 +1,4 @@
-  strAlfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var strAlfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 var strInput = "";          //input string
 var strCodeword = "";       //codeword string
@@ -11,10 +11,9 @@ arrOutputPosities = [];     //array van output posities
 //Zet een string om naar een array met posities
 function stringNaarPosities(str){
     outputArray = [];
-    for (let i = 0; i < str.length; i++){
-        for (let j = 0; j < strAlfabet.length; j++) {
-            if(str[i].toUpperCase() == strAlfabet[j]){
-                outputString.push(j);
+    for(let i = 0; i < strInput.length; i++){
+        for(let j = 0; j < strAlfabet.length; j++){
+            if(strInput[i].toUpperCase() == strAlfabet[j]){
                 // console.log("Letter: " + i + " = " + str[i] + " -> positie in alfabet = " + j);
             }
         }
@@ -28,21 +27,23 @@ function stringNaarPosities(str){
 function positiesNaarString(arr){
     var outputString = "";
     //TODO: opdracht 1
-    for(leti = 0; i < arr.length; i++){
-    for(letj = 0; j < strAlfabet.length; j++) {
-    if(arr[i] == j){
-    outputString.push(j);
-    console.log("Letter gevonden : "+ i + " = " + arr[i] + " is " + strAlfabet[j]);
-                }
-            }
-    // console.log("berekende string : " + outputString.toArray())
-    returnoutputString;
+    function positiesNaarString(arr){ 
+        var outputString = "";
+        //TODO: opdracht 1
+        for (let i = 0; i < arr.length; i++){
+             outputString = outputString + strAlfabet[ arr[i] ];   
+        }
+            //console.log("xxx" + outputString);
+        
+        return outputString;
     }
+    console.log("Letter gevonden : "+ i + " = " + arr[i] + " is " + strAlfabet[j]);
+}           
+    // console.log("berekende string : " + outputString.toArray())
     
-    console.log(outputString);
+    
 
-    return outputString;
-}
+
 
 //Geeft een array terug waarbij het codeword herhaald wordt totdat de lengte overeenkomt met het te versleutelen bericht
 //Dus als het bericht STUDENT (7 letters) is en het codeword KLAS dan krijg je KLASKLA (7 letters) terug van deze functie.
@@ -59,29 +60,31 @@ function vermenigvuldigCodeword(strInput, strCodeword){
     for(let i = 0; i < intDiv; i++){
         outputString= outputString + strCodeword;
         //console.log(outputString);
-         }
+    }
          
-        for(let j = 0; j < intMod; j++){
+    for(let j = 0; j < intMod; j++){
         outputString= outputString + strCodeword[j];
         //console.log(outputString);
-         }
+    }  
 
-
+}
 
 
    //functie die een een bericht versleuteld.
-function versleutel(){
+   function versleutel(){
     strInput = document.querySelector("#input").value; 
-    strCodeword = document.querySelector("#codeword").value; 
-
+    strCodeword = document.querySelector("#codeword").value;   
+ 
     console.log("input: " + strInput);
     console.log("codeword: " + strCodeword);
-
+ 
     strCodewordFull = vermenigvuldigCodeword(strInput, strCodeword);
-    console.log("strCodewordFull:" + strCodewordFull);
+    console.log("strCodewordFull: " + strCodewordFull);
     arrCodewordPosities = stringNaarPosities(strCodewordFull);
     arrInputPosities = stringNaarPosities(strInput);
 
+    
+    outputArray = [];
     for (let i = 0; i < arrInputPosities.length; i++) {
         if(arrInputPosities[i] + arrCodewordPosities[i] > 26){
             outputArray.push((arrInputPosities[i] + arrCodewordPosities[i])-26);
@@ -91,14 +94,12 @@ function versleutel(){
         }
         
     }
- 
     //console.log("versleuteld bericht: " + positiesNaarString(outputArray));
     // document.querySelector("#output").value = positiesNaarString(outputArray);
     strCodewordFull = [];
 }
 function ontsleutel(){
     //TODO: opdracht 3
-    strInput = document.querySelector("#input").value; 
-    strCodeword = document.querySelector("#codeword").value;   
-}
+   // strInput = document.querySelector("#input").value; 
+   // strCodeword = document.querySelector("#codeword").value;   
 }
